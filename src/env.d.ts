f65535c1cf189888bd9953e861ddfa7ebb10bd57
Erdem Özyurt <admin@erdemozyurt.com>
2026-03-19 12:42:32 +0300

    chore: remove Directus references from env types

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,8 +3,6 @@
 interface ImportMetaEnv {
   readonly SITE_URL: string;
   readonly PANEL_URL: string;
-  readonly DIRECTUS_URL?: string;
-  readonly DIRECTUS_TOKEN?: string;
 }
 
 interface ImportMeta {
